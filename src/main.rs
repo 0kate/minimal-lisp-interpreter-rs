a2bf892b656458f6456a27b9e2fc80b59312764c
0kate <o.keito317@icloud.com>
2022-11-13 17:30:44 +0900

    more improvement

--- src/main.rs
+++ src/main.rs
@@ -1,141 +1,98 @@
-use std::io::Write;
-use std::iter::Peekable;
-use std::slice::Iter;
-use std::str::Chars;
-
-fn print_prompt() {
-    print!("> ");
-    std::io::stdout().flush().expect("Failed to flush stdout.");
+#[derive(Debug)]
+enum Token {
+    LParen,
+    RParen,
+    Symbol(String),
+    Integer(i64),
 }
 
-fn read_expression(exp: &mut String) {
-    exp.clear();
-    std::io::stdin().read_line(exp).expect("Failed to read stdin.");
+#[derive(Debug)]
+enum Object {
+    Symbol(String),
+    Integer(i64),
+    List(Vec<Object>),
 }
 
-fn is_operator(ch: &char) -> bool {
-    ch == &'+' || ch == &'-' || ch == &'*' || ch == &'/'
-}
-
-fn is_word_char(ch: &char) -> bool {
-    ch.is_alphabetic() || ch.is_numeric() || is_operator(ch)
-}
-
-fn tokenize_word(chars: &mut Peekable<Chars>) -> String {
-    let mut word = String::new();
-    loop {
-        match chars.peek() {
-            Some(ch) => {
-                if is_word_char(ch) {
-                    word.push(*ch);
-                    chars.next();
-                } else {
-                    break;
-                }
-            },
-            None => break,
-        }
-    }
-
-    word
-}
-
-fn tokenize(exp: &String) -> Vec<String> {
-    let mut tokens = Vec::new();
-    let mut chars = exp.trim().chars().peekable();
+fn parse_tokens(tokens: &mut Vec<Token>) -> Vec<Object> {
+    let mut objects = Vec::new();
 
     loop {
-        match chars.peek() {
-            Some(ch) => {
-                if ch == &'(' || ch == &')' {
-                    tokens.push(ch.to_string());
-                    chars.next();
-                } else if is_word_char(ch) {
-                    tokens.push(tokenize_word(&mut chars));
-                } else {
-                    chars.next();
+        if tokens.len() < 1 { break; }
+
+        let token = Some(tokens.remove(0));
+        match token {
+            Some(token) => {
+                match token {
+                    Token::Symbol(s) => objects.push(Object::Symbol(s.to_string())),
+                    Token::Integer(i) => objects.push(Object::Integer(i)),
+                    Token::LParen => {
+                        objects.push(Object::List(parse_tokens(tokens)))
+                    },
+                    Token::RParen => break,
+                    _ => {},
                 }
             },
-            None => break,
+            None => panic!("Insufficien token"),
         }
     }
 
-    tokens
+    objects
 }
 
-fn eval_command(command: &String) {
-    match command.trim() {
-        ".exit" => {
-            std::process::exit(0);
-        },
-        _ => {},
-    }
+fn eval_symbol(sym_obj: &String) {
+    println!("eval_symbol: {:?}", sym_obj);
 }
 
-struct AST {
-    token: String,
-    left: Option<String>,
-    right: Option<String>,
+fn eval_integer(int_obj: &i64) {
+    println!("eval_integer: {:?}", int_obj);
 }
 
-impl AST {
-    fn new(token: String) -> Self {
-        Self {
-            token,
-            left: None,
-            right: None,
+fn eval_list(list_obj: &Vec<Object>) {
+    println!("eval_list: {:?}", list_obj);
+    for obj in list_obj {
+        match obj {
+            Object::Symbol(s) => eval_symbol(&s),
+            Object::Integer(n) => eval_integer(&n),
+            Object::List(l) => eval_list(&l),
         }
     }
-
-    fn add_left(&mut self, left: String) {
-        self.left = Some(left);
-    }
-
-    fn add_right(&mut self, right: String) {
-        self.right = Some(right);
-    }
 }
 
-fn parse_tokens(tokens: &mut Peekable<Iter<String>>) -> AST {
-    let mut ast = AST::new(tokens.next().unwrap().to_string());
-
-    ast.add_left(tokens.next().unwrap().to_string());
-    ast.add_right(tokens.next().unwrap().to_string());
-
-    ast
-}
-
-fn eval_ast(ast: &AST) {
-    match ast.token.as_str() {
-        "+" => {
-            let left_val: u32 = ast.left.as_ref().unwrap().parse().unwrap();
-            let right_val: u32 = ast.right.as_ref().unwrap().parse().unwrap();
-            println!("{}", left_val + right_val);
-        },
-        _ => {},
+fn eval(obj: &Object) {
+    match obj {
+        Object::Symbol(s) => eval_symbol(&s),
+        Object::Integer(n) => eval_integer(&n),
+        Object::List(l) => eval_list(&l),
     }
-}
-
-fn eval_expression(exp: &String) {
-    let tokens = tokenize(exp);
-    let mut tokens_iter = tokens.iter().peekable();
-
-    tokens_iter.next();  // skip '('
-    let ast = parse_tokens(&mut tokens_iter);
-    eval_ast(&ast);
 }
 
 fn main() {
-    let mut input = String::new();
+    let mut text = String::new();
+    std::io::stdin().read_line(&mut text).expect("Failed to read stdin.");
 
-    loop {
-        print_prompt();
-        read_expression(&mut input);
+    text = text.trim().to_string()
+        .replace("(", " ( ")
+        .replace(")", " ) ");
+    let words = text.split_whitespace();
 
-        if input.starts_with('.') {
-            eval_command(&input);
-        } else {
-            eval_expression(&input);
+    let mut tokens = Vec::new();
+    for word in words {
+        match word {
+            "(" => tokens.push(Token::LParen),
+            ")" => tokens.push(Token::RParen),
+            _ => {
+                if let Ok(num) = word.parse::<i64>() {
+                    tokens.push(Token::Integer(num));
+                } else {
+                    tokens.push(Token::Symbol(word.to_string()));
+                }
+            },
         }
     }
+    println!("tokens: {:?}", tokens);
+
+    let objects = parse_tokens(&mut tokens);
+    println!("objects: {:?}", objects);
+
+    eval(&objects[0]);
 }
